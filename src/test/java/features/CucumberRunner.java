cd1925ca177bc3596efa2f72ecb5b148560dbf57
mehtaperguven <mehtaperguven13@gmail.com>
2020-06-02 21:09:35 -0500

    change is done

--- src/test/java/features/CucumberRunner.java
+++ src/test/java/features/CucumberRunner.java
@@ -6,4 +6,11 @@ public class CucumberRunner {
     Karate metaWeatherTest(){
         return new Karate().tags("@meta_weather").relativeTo(getClass());
     }
+    @Karate.Test
+    Karate bookItTest(){
+        return new Karate().tags("@bookit").relativeTo(getClass());
+    }
 }
+
+
+//https://intuit.github.io/karate/#karate-configjs
